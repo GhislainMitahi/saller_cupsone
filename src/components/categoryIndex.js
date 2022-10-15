5a10032a0d6eff5884f3dc798a6be025d49f8f8b
Ghislain Mitahi <ghislainmitahi@gmail.com>
2022-10-15 14:08:47 +0200

    edit categorie

--- src/components/categoryIndex.js
+++ src/components/categoryIndex.js
@@ -9,8 +9,8 @@ const CategoryIndex = () => {
   const { jobs } = useSelector(selectAllJobs);
   const check = jobs.filter((x) => x.id === id);
   return (
-    <div className="flex justify-center flex-wrap px-4 gap-2 w-full relative">
-      <Link to="/" className="text-4xl text-blue-600 absolute left-16 py-2">Go Back Home</Link>
+    <div className="flex justify-center flex-wrap px-4 gap-2 w-full relative ">
+      <Link to="/" className="text-4xl text-white absolute left-16 py-2">Go Back Home</Link>
       {jobs.length > 0 ? (
         check.length !== 0 ? (
           check.map((x) => {
@@ -29,23 +29,23 @@ const CategoryIndex = () => {
                 className="flex justify-center w-11/12 relative top-16"
                 key={id}
               >
-                <div className="block p-6 rounded-lg shadow-lg bg-white w-64 lg:8/12 grow">
+                <div className="block p-6 rounded-lg shadow-lg bg-[#ec4c8b] w-64 lg:8/12 grow">
                   <div className="border-b px-2 py-1 mb-4 flex flex-col">
-                    <span className="text-xs font-thin text-slate-400">
+                    <span className="text-xs font-thin text-white">
                       {location.display_name}
                     </span>
-                    <span className="text-xs font-thin text-slate-400">
+                    <span className="text-xs font-thin text-white">
                       {company.display_name}
                     </span>
-                    <span className="text-xs font-thin text-slate-400">
+                    <span className="text-xs font-thin text-white">
                       {category.label}
                     </span>
                   </div>
-                  <h2 className="text-gray-900 text-base leading-tight font-semibold mb-2">
+                  <h2 className="text-white text-base leading-tight font-semibold mb-2">
                     {title}
                   </h2>
-                  <p className="text-gray-700 text-base mb-4">{description}</p>
-                  <p className="text-base font-semibold text-black mb-4">
+                  <p className="text-white text-base mb-4">{description}</p>
+                  <p className="text-base font-semibold text-white mb-4">
                     Salary:
                     {salary_is_predicted !== '0' ? (
                       `$ ${x.salary_max}`
@@ -58,7 +58,7 @@ const CategoryIndex = () => {
                   <a
                     href={redirect_url}
                     target="_blank"
-                    className="bg-blue-600 text-white px-4 py-2 rounded-md mt-3"
+                    className="bg-gray-100 text-blue-600 px-4 py-2 rounded-md mt-3"
                     rel="noreferrer"
                   >
                     Apply
