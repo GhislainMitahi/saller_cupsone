56f9ccc22598cc89d361fff932780ff9fbb64055
Ghislain Mitahi <ghislainmitahi@gmail.com>
2022-10-15 14:09:13 +0200

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -42,7 +42,7 @@ const Header = () => {
                   aria-hidden="true"
                   focusable="false"
                   data-prefix="fas"
-                  className="w-5"
+                  className="w-5 text-white"
                   role="img"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 448 512"
